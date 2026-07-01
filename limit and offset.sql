b21f7b52c204206311267093d1412f674a092d43
mahadi40011 <mahadi40011@gmail.com>
2026-07-01 20:02:31 +0600

    Practicing Limit and offset Commands

---
+++ limit and offset.sql
@@ -0,0 +1,9 @@
+select * from students
+
+select * from students limit 5
+
+select * from students limit 5 offset 5
+  
+select * from students limit 5 offset 5*0
+select * from students limit 5 offset 5*1
+select * from students limit 5 offset 5*2
